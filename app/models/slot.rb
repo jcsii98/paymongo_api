576c573bc2292f3b71f76d7f29d4bddb7bbe9c55
Jose Saribong <jose.c.saribong@gmail.com>
2023-11-29 14:24:19 +0800

    remove unused method in slot.rb

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,11 +1,4 @@
 class Slot < ApplicationRecord
     has_many :tickets, dependent: :destroy
 
-    def update_slot_status
-        if self.status === 'available'
-            update(status: 'occupied')
-        else
-            update(status: 'available')
-        end
-    end
 end
